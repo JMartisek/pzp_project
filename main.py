37dd6f4bc6f4aeae9aad5dd442d009b1263d0f45
JMartisek <jirkamartisek142@gmail.com>
2023-12-08 10:40:29 +0100

    add graphs

--- main.py
+++ main.py
@@ -22,10 +22,17 @@ def statistic(SizeData, StopWordsData, sizeTime, stopTime):
 
 
 # graphic function
-def pieChart(labels, size):
-    fig, ax = plt.subplots()
-    ax.pie(size, labels=labels, autopct='%1.1f%%')
-    plt.show()
+def pieChart(labels, size, title, index):
+    #plt.figure(index)
+    fig, axs = plt.subplots(1,2+index)
+    for i in range(2+index):
+        axs[i].pie(size[i], labels=labels, autopct='%1.1f%%')
+        axs[i].set_title(title[i])
+    if(index == 1):
+        plt.show(block=False)
+    else:
+        plt.show()
+
 
 
 # data load
@@ -69,5 +76,17 @@ print("------------------------ SPARK -------------------------")
 [filteredSparkSizeData, SparkSizeInterval] = SparkSize(parsedData)
 [filteredSparkStopData, SparkStopInterval] = SparkStopWords(parsedData, stopData)
 statistic(filteredSparkSizeData, filteredSparkStopData, SparkSizeInterval, SparkStopInterval)
+
+
+
+pieChart(["CPU single thread", "CPU multithread", "GPU", "Spark"],
+         [[cpuSingleThreadData[1]+cpuSingleThreadData[3],CPU_size_interval+CPU_stop_interval,
+           SizeInterval+StopInterval, SparkSizeInterval + SparkStopInterval],[cpuSingleThreadData[1],CPU_size_interval,
+        SizeInterval, SparkSizeInterval],[cpuSingleThreadData[3],CPU_stop_interval,StopInterval, SparkStopInterval]],
+         ["Filter size and stop word","Filter size","Filter stop word"], 1)
+
+pieChart(["Filter size", "Filter stopWords"], [[cpuSingleThreadData[1],cpuSingleThreadData[3]],[CPU_size_interval,CPU_stop_interval],[SizeInterval,StopInterval],[SparkSizeInterval,SparkStopInterval]],["CPU single thread", "CPU multithread","GPU", "Spark"],2)
+
+
 
 input('Press ENTER to exit')
